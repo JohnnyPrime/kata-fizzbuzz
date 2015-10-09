0b37fc55f4b522779919a315cffe08ca7b4dc43d
MATC <MATC>
2015-10-09 10:13:53 -0600

    html implementation with jquery

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,31 +1,45 @@
-/*globals exports*/
+/*globals $, document, event*/
 
-var fizzBuzz = function (nInput) {
-    var list = '';
+$(document).ready(function () {
 
-    for (var i = 0; i < nInput; i++) {
 
-        if (i === 0) {
-            list = list.concat("0");
-        } else if (i % 15 === 0) {
-            list = list.concat(", fizzbuzz");
-            //          return "fizzbuzz";
-        } else if (i % 3 === 0) {
-            list = list.concat(", fizz");
-            //          return "fizz";
-        } else if (i % 5 === 0) {
-            list = list.concat(", buzz");
-            //           return "buzz";
-        } else {
-            list = list.concat(", " + i);
-            //          return i;
+    var fizzBuzz = function (nInput) {
+        var list = '';
+
+        for (var i = 0; i <= nInput; i++) {
+
+            if (i === 0) {
+                list = list.concat("0");
+            } else if (i % 15 === 0) {
+                list = list.concat(", fizzbuzz");
+                //          return "fizzbuzz";
+            } else if (i % 3 === 0) {
+                list = list.concat(", fizz");
+                //          return "fizz";
+            } else if (i % 5 === 0) {
+                list = list.concat(", buzz");
+                //           return "buzz";
+            } else {
+                list = list.concat(", " + i);
+                //          return i;
+            }
+
         }
+        list = list.concat('</p>');
+        return list;
+    };
 
-    }
-    console.log(list);
-};
 
-fizzBuzz(30);
+    $("#button").on('click', function () {
+        event.preventDefault();
+        var p = $("#input").val();
+        var r = fizzBuzz(p);
+        $('.fizzBlock').append('<p><b>Results Fo\' 0 - ' + p + ":</b> " + r);
+
+    });
+});
+
+
 
 
 //exports.fizzBuzz = fizzBuzz;
